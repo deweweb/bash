8e0295b9c5c2b23500d74e54ad720d71b7181c27
deweweb <deweweb@gmail.com>
2019-02-28 14:00:58 +0100

    Update delete_from_folder.sh

--- delete_from_folder.sh
+++ delete_from_folder.sh
@@ -5,9 +5,9 @@
 
 file=*2017_09_04*
 
-##cd /data/products/ec/data/datenaufbereitung/ablage/trt
+##cd /data/products/../ablage/trt
 
-cd /data/products/ec/logs
+cd /data/products/../logs
 
 for f in *; do
 	if [ -e "$f" ]; then
